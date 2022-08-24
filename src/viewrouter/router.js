4c10136a8cde152d8905b6ffd35db98720e3ef23
Herly <herlyyidam@gmail.com>
2022-08-23 21:33:01 -0500

    Redirigir a home cuando el navegador no reciba nada, con el switch

--- src/viewrouter/router.js
+++ src/viewrouter/router.js
@@ -1,20 +1,21 @@
 // importar todas mis vistas
 import { components } from "../lib/index.js";
 
+// eslint-disable-next-line consistent-return
 const changeView = (route) => {
   const root = document.querySelector("#root");
   root.innerHTML = "";
   switch (route) {
     // eslint-disable-next-line no-lone-blocks
-    case "#/home": { return root.appendChild(components.viewHome()); }
+   // case "#/home": { return root.appendChild(components.viewHome()); }
     // eslint-disable-next-line no-lone-blocks
     case "#/register": { return root.appendChild(components.viewRegist()); }
     // root.textContent = "home"; // renderizar las vistas dentro del root
+    case "#/home":
     default:
-      break;
+      return root.appendChild(components.viewHome());
 
       // root.textContent = "404";
   }
-  
 };
 export { changeView };
