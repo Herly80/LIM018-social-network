07f27e86318ca950e07c257fa539562beecee085
Herly <herlyyidam@gmail.com>
2022-08-30 12:52:26 -0500

    tratando de enlazar al repositorio remoto github

--- src/View/home.js
+++ src/View/home.js
@@ -3,8 +3,8 @@ export default () => {
   <div class="boxForm">
   <h1>Tribu Saludable</h1>
   <form>
-  <input type="email" name="" id="correo" placeholder="E-mail">
-  <input type="password" name="" id="contraseña" placeholder="Contraseña">
+  <input type="email" name="" class="input" id="correo" placeholder="E-mail">
+  <input type="password" name="" class="input" id="contraseña" placeholder="Contraseña">
   <button id= "sesion" class="iniciar">Iniciar Sesión</button>
   <button id= "ingGoogle" class="ingGoogle">Ingresa con Google</button>
   <p id="pregunta">¿No tienes Cuenta?</p>
--- src/View/register.js
+++ src/View/register.js
@@ -2,13 +2,15 @@ export default () => {
   const viewregist = `
   <a href="#/home">Inicio</a>
   <a href="#/access" id="go">Adelante</a>
-  <div class="boxRegist">
+  <div class="boxForm">
   <h1 id= "cuenta" >Registrate aqui</h1>
-  <input type="name" name="" id="name" placeholder="Nombre">
-  <input type="email" name="" id="emailReg" placeholder="E-mail">
-  <input type="password" name="" id="passwordReg" placeholder="Contraseña">
+  <form>
+  <input type="name" name="" class="input" id="name" placeholder="Nombre">
+  <input type="email" name="" class= "input" id="emailReg" placeholder="E-mail">
+  <input type="password" name="" class="input" id="passwordReg" placeholder="Contraseña">
   <button class="registrar">Registrarme</button>
   <button id= "regGoogle" class="regGoogle">Registrarme con Google</button>
+  </form>
   </div> `;
 
   const cajaPrincipal = document.createElement("div");
